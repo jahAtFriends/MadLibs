b8de36c05a57cfeab90d5a4aee483640d0073432
jahAtFriends <jhammer@friendsbalt.org>
2021-09-13 09:11:43 -0400

    starter code

--- MadLib.java
+++ MadLib.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
-
 public class MadLib {
     
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
         
+        //Welcome the user.
+        System.out.println("Hello! Welcome to the Mad Lib!");
+        
+        System.out.println("What's your name?");
+        String userName = input.next();
+        
+        System.out.println("How old are you?");
+        int userAge = input.nextInt();
+        
+        System.out.println("Wow! " + userName + " is " + userAge + " years old!");    
     }
 }
